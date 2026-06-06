9fe97e0c11589a4da316e9baf152337c49c574d8
Z User <z@container>
2026-06-06 20:55:48 +0000

    0f37df7a-bf34-414f-91ef-70a6e29b05ba

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7O47G7ekKYSdS_uAAnyov7RHK0m68ETY",
-  authDomain: "apple-net-df0e7.firebaseapp.com",
-  databaseURL: "https://apple-net-df0e7-default-rtdb.firebaseio.com",
-  projectId: "apple-net-df0e7",
-  storageBucket: "apple-net-df0e7.firebasestorage.app",
-  messagingSenderId: "910060697351",
-  appId: "1:910060697351:web:4k86gfb72msn1u5u",
-  measurementId: "G-XXXXXXXXXX",
+  apiKey: "AIzaSyDeQMrepTnlldqGycyMzy1qeoaD3g7nxgA",
+  authDomain: "applenet711.firebaseapp.com",
+  databaseURL: "https://applenet711-default-rtdb.firebaseio.com",
+  projectId: "applenet711",
+  storageBucket: "applenet711.firebasestorage.app",
+  messagingSenderId: "164323561264",
+  appId: "1:164323561264:web:2000f0cc595b6d7260c2f5",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
